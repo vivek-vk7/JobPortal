73d3ba4b9123bb72c1ad7f97e80e139743f47f31
vivek-vk7 <imvivek2005@gmial.com>
2026-04-19 01:54:08 +0530

    updated login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -113,17 +113,20 @@ export const updateProfile = async (req,res) => {
     try {
         const {fullname, email,phoneNumber,bio, skills} = req.body;
         const file = req.file;
-        if(!fullname || !email || !phoneNumber || !bio || !skills) {
-            return res.status(400).json({
-                message: "Something is missing",
-                success: false
-            });
-        };
+        // if(!fullname || !email || !phoneNumber || !bio || !skills) {
+        //     return res.status(400).json({
+        //         message: "Something is missing",
+        //         success: false
+        //     });
+        // };
 
         // cloudinary logic to upload resume and get url will come here later
 
+        let skillsArray;
+        if(skills){
+            skillsArray = skills.split(",");
+        }
 
-        const skillsArray = skills.split(",");
         const userId = req.id;  // req.id is set in auth middleware after verifying token
         let user = await User.findById(userId);
         if(!user) {
@@ -133,11 +136,12 @@ export const updateProfile = async (req,res) => {
             });
         }
         //updating data
-        user.fullname = fullname,
-        user.email = email,
-        user.phoneNumber = phoneNumber,
-        user.profile.bio = bio,
-        user.profile.skills = skillsArray
+        if(fullname) user.fullname = fullname;
+        if(email) user.email = email;
+        if(phoneNumber) user.phoneNumber = phoneNumber;
+        if(bio) user.profile.bio = bio;
+        if(skills) user.profile.skills = skillsArray;
+        
         //resume comes later here...
 
 
